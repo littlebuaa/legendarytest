0d79754b20a84f68413450dedacd34a3f9ac54ef
LT <tuo.li@zoov.eu>
2018-10-15 15:02:29 +0200

    update setup tools, to remove unnecessary files after creation of tar.gz file.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 # -*- coding:utf-8 -*-
 
 
-from setuptools import setup,find_packages
+from setuptools import setup,find_packages,Command
+import shutil
 import os
 import sys
 import subprocess
@@ -12,6 +13,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 about = {}
 with open(os.path.join(here, 'framework', '__version__.py')) as f:
     exec(f.read(), about)
+project_name = "legendarytest"
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        print("done")
+        shutil.rmtree('{}.egg-info'.format(project_name))
 
 
 def call(msg, commands, error_msg, exit_on_fail=False):
@@ -38,4 +51,5 @@ setup(name='legendarytest',
           'colorama',
       ],
       long_description=open('README.md').read(),
+      cmdclass={'clean': CleanCommand},
       zip_safe=False,)
