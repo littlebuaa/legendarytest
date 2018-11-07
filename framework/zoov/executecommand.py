127b95c965123e384d7579020abfb8852dca6acd
LT <tuo.de.li@gmail.com>
2018-11-07 17:13:02 +0100

    Command Execution test, add timeout as parameter

--- framework/zoov/executecommand.py
+++ framework/zoov/executecommand.py
@@ -1,15 +1,15 @@
 from framework.common.testing import Test
 
 class ExecuteCommand(Test):
-    def __init__(self, dut, command):
-        super().__init__(dut, "execute command " + command)
+    def __init__(self, dut, command, timeout=10000):
+        super().__init__(dut, "exec" + command)
         self.command = command
+        self.timeout = timeout
 
     def test(self):
-        return_code, text = self.dut.execute_command(self.command)
+        return_code, text = self.dut.execute_command(self.command,self.timeout)
         if return_code != 0:
             self.logger.info("CSVFILE " + self.name.replace(" ", "_") + " ok fail fail")
             return False
         self.logger.info("CSVFILE " + self.name.replace(" ", "_") + " ok ok pass")
-        input("--------------- Reboot the BOARD ---------!!!!")
         return True
