fcfc410dda38d68f8349ffa46a88bbe11452d991
LT <tuo.de.li@gmail.com>
2019-01-04 17:59:41 +0100

    update test script, manage prod mode and diagnostic mode

--- toto.py
+++ toto.py
@@ -1,10 +1,13 @@
-"""T30 station (semi-finished product test)"""
+# -*- coding: utf-8 -*-
 
-from framework.tools.device import DUT
+import sys
+sys.path.insert(0,'../../')
+from framework.tools.device import Zoovstation as Zoov
 import framework.tools.utils as TOOL
-from framework.common import *
+from framework.zoov import *
 from framework.tools.config import configget
 from framework.tools import logger as logtool
+from framework.tools.utils import ENCODING, get_encoding
 
 
 def print_test_summary(oklist, nglist):
@@ -31,51 +34,77 @@ def print_test_summary(oklist, nglist):
     print('\n')
     print('{:#^80}\n'.format("   End of Summary   "))
 
-def main():
 
+def main (isProd = True):
+    """Define code in main to avoid execution if it is imported."""
     ######################  init test       ######################
     time_begin = TOOL.init_test()
     test_result = True
 
-    ######################  scan MAC address    ######################
-    mac_scan = TOOL.scan_mac("Please scan the MAC label:")
-
-
     ######################  create log handler  ######################
-    log_file_name = logtool.log_name_gen("txt", mac_scan)
+    serialID = TOOL.scan_serial_id("請掃描或輸入車身QR二維碼，Please scan the bike QR code:")
+
+    log_file_name = logtool.log_name_gen("txt", serialID)
     logtool.init_logger(log_file_name)
 
-
-    ######################  ready to power on   ######################
-    print("Power on the device under test, then press ENTER to continue...")
-    input()
     ######################  create DUT          ######################
-    dut = DUT("DUT", configget("COM", "dut"))
+    comport = configget("COM", "zoovstation")
+    print(comport, serialID)
+    dut = Zoov(comport,serialID)
     if not dut.open():
         print("ERROR","Failed to open COM port!")
         exit()
     ######################  create test sequence ######################
+
     TOOL.flush_input()
 
     tests = [
-        Reset(dut),
-        WaitShell(dut),
-        # CheckWiFiPER(dut)
-        # CheckMicrophone(dut)
+        WaitShellConnect(dut),
+        # CheckBoardID_A100(dut),
+        # SetSerialID(dut),
+        # CheckBrake(dut),
+        # CheckBatteryDetect(dut),
+        CheckPowerLED(dut),
+        # ExecuteCommand(dut,'low_power_check'),
+        # CheckLight(dut),
+        # ExecuteCommand(dut,'torque_calibration'),
+        # MotorCalibration(dut),
+        # ExecuteCommand(dut,"factory_mode_set --mode 3"),
+        # ExecuteCommand(dut,"reboot")
+		SetGateway(dut),
     ]
 
     ######################      RUN the test     ######################
-    for test in tests:
-        if not test.run():
+    ## Production mode and Dianostic mode
+    if isProd:
+        for test in tests:
+            if not test.run():
+                test_result = False
+                break
+    else:
+        oklist =[]
+        nglist = []
+        for test in tests:
+            if not test.run():
+                nglist.append(test)
+            else:
+                oklist.append(test)
+        if len(nglist) != 0:
             test_result = False
-            break
 
     ######################      Closing          ######################
-    dut.close()
+    # dut.close()
     TOOL.flush_input()
-    csvinfo = logtool.get_csv_info(test_result, mac_scan, time_begin)
+    csvinfo = logtool.get_csv_info(test_result, serialID, time_begin)
     logtool.csvmaker(log_file_name, csvinfo)
-    TOOL.show_result(test_result)
+
+
+    ######################      Display Result    #####################
+    TOOL.show_result(test_result,pop = isProd)
+    if not isProd:
+        print_test_summary(oklist, nglist)
+
+
 
 if __name__ == "__main__":
     main()
